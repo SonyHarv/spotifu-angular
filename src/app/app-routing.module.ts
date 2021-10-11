0bc9e83344b58d4e4e408aeb1d29a362e6658b58
Erikson TF <sonyharv9@gmail.com>
2021-10-10 22:45:32 -0700

    Add path for ExampleComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', //TODO:localhost:4200/ <--
+    component: ExampleComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
