8529bf049903077040470ad75bbebe3636f23efb
Erikson TF <sonyharv9@gmail.com>
2021-10-12 23:10:27 -0700

    Add loadChildren for the routing home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '', //TODO:localhost:4200/ <--
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
   },
 ];
 
